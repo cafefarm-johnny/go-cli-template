1353ec0a5fe71d6e21bd5b1fe2c4501642b5c40b
Johnny <npcdja@mrblue.com>
2021-07-06 14:44:56 +0900

    feat: cmd help 템플릿 추가 및 --help 플래그 제거,

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -2,17 +2,20 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"go-cli-template/src/cmd/template"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:           "my-cli",
 	Short:         "cobra cli template project.",
-	Example:       "my-cli [command] [-flags?]",
-	SilenceErrors: true,
+	Example:       "  my-cli [command] [-flags?]",
+	SilenceUsage:  false, // usage 미출력 옵션 (에러 출력 시 usage가 함께 출력되므로 비활성화 처리)
+	SilenceErrors: true,  // 에러 미출력 옵션 (RunE 함수에서 에러 반환 시 에러가 두번 출력되므로 비활성화 처리)
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("  cobra cli tmepate project. if you want help my-cli help")
+		fmt.Println(`Usage: my-cli [command] if you want help? just enter "my-cli help"`)
 	},
 }
 
@@ -24,5 +27,10 @@ func Exec() {
 }
 
 func init() {
+	rootCmd.SetHelpTemplate(template.HelpTemplate)
+
+	rootCmd.Flags().BoolVarP(&help, "help", "h", false, "")
+	rootCmd.Flags().Lookup("help").Hidden = true
+
 	rootCmd.AddCommand(versionCmd)
 }
--- src/cmd/version.go
+++ src/cmd/version.go
@@ -2,14 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"go-cli-template/src/app"
+
+	"github.com/spf13/cobra"
 )
 
 var versionCmd = &cobra.Command{
 	Use:     "version",
-	Short:   "  print the application version.",
-	Example: "my-cli version",
+	Short:   "print the application version.",
+	Example: "  my-cli version",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(fmt.Sprintf("%s (ver %s)", app.Name, app.Version))
 	},
